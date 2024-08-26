220b5195a14ca1f924e044ec4d99ed2734ce3be0
Arul Karthikeyan.S <125200375+Arul2K23@users.noreply.github.com>
2024-08-26 09:40:03 +0530

    Update Home.jsx

--- ToDolist/src/Home.jsx
+++ ToDolist/src/Home.jsx
@@ -7,13 +7,13 @@ import { BsCircleFill, BsFillCheckCircleFill } from "react-icons/bs";
 function Home(){
         const [todos,setTodos] = useState([])
         useEffect(() => {
-            axios.get('http://localhost:3001/get')
+            axios.get('https://todo-list-api-dun.vercel.app/get')
             .then(result => setTodos(result.data))
             .catch(err => console.log(err))
         },[])
 
     const handleEdit = (id) =>{
-        axios.put('http://localhost:3001/update/'+id)
+        axios.put('https://todo-list-api-dun.vercel.app/update/'+id)
             .then(result =>  {
                 location.reload()
             })
@@ -21,7 +21,7 @@ function Home(){
     }
 
     const handleDelete= (id)=>{
-        axios.delete('http://localhost:3001/delete/'+id)
+        axios.delete('https://todo-list-api-dun.vercel.app/delete/'+id)
             .then(result =>  {
                 location.reload()
             })
